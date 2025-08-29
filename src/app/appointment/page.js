5440021d6780fd924cc86587f6c412d00057abdc
harshilpec17 <harshilsuthar1995@gmail.com>
2025-08-29 00:53:09 -0400

    refactor: Remove Navbar component from Services page

--- src/app/appointment/page.js
+++ src/app/appointment/page.js
@@ -1,9 +1,6 @@
-import Navbar from "@/components/Navbar.js";
-
 export default function Services() {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <main className="pt-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <h1 className="sr-only">Services</h1>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import Navbar from "./components/Navbar";
-import DynamicHero from "./components/DynamicHero";
-import Footer from "./components/Footer.jsx";
+import Navbar from "@/components/Navbar";
+import DynamicHero from "@/components/DynamicHero";
+import Footer from "@/components/Footer.jsx";
 
 export default function Home() {
   return (
